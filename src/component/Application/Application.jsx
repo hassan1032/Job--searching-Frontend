16a9c80375f94f9f2b424aa70bb52cf07e4783a6
hassan1032 <khanhassan1032@gmail.com>
2024-06-03 12:26:21 +0530

    Add Chenges

--- src/component/Application/Application.jsx
+++ src/component/Application/Application.jsx
@@ -53,7 +53,7 @@ const Application = () => {
       toast.success(data.message);
       navigateTo("/jobs");
     } catch (error) {
-      toast.error(error.response.data.message);
+      toast.error(error.response?.data?.message );
     }
   };
 
